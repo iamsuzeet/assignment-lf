ba6b96e4a29c5a59d45dc58fa3016af1129f3672
iamsuzeet <iamsuzeet@gmail.com>
2019-12-15 17:48:34 +0545

    helper color function to set color and get color index and color

---
+++ final-project-js/js/colors.js
@@ -0,0 +1,11 @@
+/**
+ * return random color;
+ */
+var colors = ['#FF3399', '#33FFFF', '#FFFF33', '#AA00FF'];
+
+export function getColor(index) {
+  var n = index;
+  return colors[n % 4];
+}
+
+export var colorIndex = Math.floor(4 * Math.random());
