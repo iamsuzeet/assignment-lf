1794ed6b184725c6bc98f7b345c1387f92558efc
iamsuzeet <iamsuzeet@gmail.com>
2019-12-18 17:24:56 +0545

    added random color generator function

--- final-project-js/js/colors.js
+++ final-project-js/js/colors.js
@@ -1,8 +1,15 @@
 /**
- * return random color;
+ * files for changing color and color utilities
  */
+
 export var colors = ['#FF3399', '#33FFFF', '#FFFF33', '#AA00FF'];
 
+//return random color
+export function randomColor() {
+  return colors[Math.floor(4 * Math.random())];
+}
+
+//return color when given index
 export function getColor(index) {
   var n = index;
   return colors[n % 4];
