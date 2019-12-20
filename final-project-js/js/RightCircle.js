6f7e1bf70d44a0c09915092f08bf26e9616ceb96
iamsuzeet <iamsuzeet@gmail.com>
2019-12-20 08:38:42 +0545

    instance for outer circle and right circle

--- final-project-js/js/RightCircle.js
+++ final-project-js/js/RightCircle.js
@@ -5,7 +5,7 @@ import { coordinates, modAngle, getDistanceAngle } from './utility.js';
 import { getColor } from './colors.js';
 
 export default class RightCircle {
-  constructor(game, radius, speed, direction, posx, color) {
+  constructor(game, radius, speed, direction, posx) {
     this.posx = posx;
     this.game = game;
     this.ctx = this.game.ctx;
@@ -27,23 +27,6 @@ export default class RightCircle {
       this.game
     );
 
-    this.circle.color = color;
-
-    switch (this.circle.color) {
-      case 0:
-        this.circle.color = 3;
-        break;
-      case 1:
-        this.circle.color = 2;
-        break;
-      case 2:
-        this.circle.color = 1;
-        break;
-      case 3:
-        this.circle.color = 0;
-        break;
-    }
-
     this.addCircleProps();
   }
 
@@ -53,7 +36,7 @@ export default class RightCircle {
     this.circle.width = (this.circle.radius * 15) / 100;
 
     this.circle.draw = () => {
-      var coord = coordinates(
+      let coord = coordinates(
         this.circle.posx,
         this.circle.posy,
         this.game.gameCanvasHeight,
@@ -62,7 +45,7 @@ export default class RightCircle {
 
       this.ctx.lineWidth = this.circle.width;
 
-      for (var j = 0; j < 4; j++) {
+      for (let j = 0; j < 4; j++) {
         this.ctx.beginPath();
 
         this.ctx.strokeStyle = getColor(j + this.circle.color);
@@ -72,14 +55,14 @@ export default class RightCircle {
          * a = 180 , a2 = 270
          * a = 270 , a2 = 360
          */
-        var a = modAngle(this.circle.angle + (Math.PI / 2) * j);
-        var a2 = modAngle(a + Math.PI / 2);
+        let a = modAngle(this.circle.angle + (Math.PI / 2) * j);
+        let a2 = modAngle(a + Math.PI / 2);
         if (
           getColor(j + this.circle.color) != this.playerInfo.color &&
           !this.game.gameOver
         ) {
           //get distance and angle from two coordinates
-          var dots = getDistanceAngle(
+          let dots = getDistanceAngle(
             coord,
             coordinates(
               this.playerInfo.posx,
@@ -96,8 +79,10 @@ export default class RightCircle {
             dots.distance + this.playerInfo.radius >
               this.circle.radius - this.circle.width / 2
           ) {
-            var positiveAngle = modAngle(-dots.angle);
+            let positiveAngle = modAngle(-dots.angle);
+            //check collison between angle point
             if (positiveAngle > a && positiveAngle < a2) {
+              //destroy/scatter player circle
               scatterPlayer(this.game);
             }
           }
@@ -105,6 +90,7 @@ export default class RightCircle {
         this.ctx.arc(coord.x, coord.y, this.circle.radius, a, a2);
         this.ctx.stroke();
       }
+      //rotation of each arc
       this.circle.angle += (this.circle.speed * Math.PI) / 180;
     };
   }
